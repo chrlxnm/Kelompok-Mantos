75e899d0842b448030a3aa9a79549be0f37fc93e
CHOIRUL ANAM <choianam1987@gmail.com>
2022-09-27 11:02:17 +0700

    add content

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,21 @@
+import { Breadcrumb } from 'antd';
 import React from 'react';
 
 const Home = (props) => {
-    return (
+    return (<>
+        <Breadcrumb
+          style={{
+            margin: '16px 0',
+          }}
+        >
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
         <div className="d-flex justify-content-center align-items-center vh-100">
             <h1> Home Page </h1>
         </div>
+        </>
     );
 };
 
