42e2651c7c6b95cfabf6bb28d3d01190ff51fd16
CHOIRUL ANAM <choianam1987@gmail.com>
2022-09-27 11:48:09 +0700

    katalog home page

---
+++ src/helpers/constant.js
@@ -0,0 +1,102 @@
+
+export const APARTMENT_DIRECTION = {
+    NORTH : "NORTH",
+    NORTHEAST : "NORTHEAST",
+    EAST : "EAST",
+    SOUTHEAST : "SOUTHEAST",
+    SOUTH : "SOUTH",
+    SOUTHWEST : "SOUTHWEST",
+    WEST : "WEST",
+    NORTHWEST : "NORTHWEST",
+}
+
+export const APARTMENT_STATUS = {
+    AVAILABLE : "AVAILABLE",
+    RENTED : "RENTED",
+    SOLD : "SOLD",
+    UNAVAILABLE : "UNAVAILABLE",
+}
+
+
+export const APARTMENT_RENT_SCHEMA = {
+    DAILY : "DAILY",
+    WEEKLY : "WEEKLY",
+    MONTHLY : "MONTHLY",
+}
+
+export const MARITAL_STATUS = {
+    SINGLE : 'SINGLE',
+    TAKEN : 'TAKEN',
+    MARRIED : 'MARRIED',
+    DIVORCED : 'DIVORCED',
+    JONES : 'JONES',
+}
+
+
+export const DUMMY_LIST = [
+    {
+        id: 1,
+        unitCode: 'A01',
+        floor: 17,
+        rooms: 87,
+        direction: APARTMENT_DIRECTION.NORTH,
+        status: APARTMENT_STATUS.AVAILABLE,
+        balcony: true,
+        furnished: true,
+        rentPrice: 500000,
+        rentSchema: APARTMENT_RENT_SCHEMA.MONTHLY,
+        sellPrice: 2000000,
+    },
+    {
+        id: 2,
+        unitCode: 'A01',
+        floor: 17,
+        rooms: 87,
+        direction: APARTMENT_DIRECTION.NORTH,
+        status: APARTMENT_STATUS.AVAILABLE,
+        balcony: true,
+        furnished: true,
+        rentPrice: 500000,
+        rentSchema: APARTMENT_RENT_SCHEMA.MONTHLY,
+        sellPrice: 2000000,
+    },
+    {
+        id: 3,
+        unitCode: 'A01',
+        floor: 17,
+        rooms: 87,
+        direction: APARTMENT_DIRECTION.NORTH,
+        status: APARTMENT_STATUS.AVAILABLE,
+        balcony: true,
+        furnished: true,
+        rentPrice: 500000,
+        rentSchema: APARTMENT_RENT_SCHEMA.MONTHLY,
+        sellPrice: 2000000,
+    },
+    {
+        id: 4,
+        unitCode: 'A01',
+        floor: 17,
+        rooms: 87,
+        direction: APARTMENT_DIRECTION.NORTH,
+        status: APARTMENT_STATUS.AVAILABLE,
+        balcony: true,
+        furnished: true,
+        rentPrice: 500000,
+        rentSchema: APARTMENT_RENT_SCHEMA.MONTHLY,
+        sellPrice: 2000000,
+    },
+    {
+        id: 5,
+        unitCode: 'A01',
+        floor: 17,
+        rooms: 87,
+        direction: APARTMENT_DIRECTION.NORTH,
+        status: APARTMENT_STATUS.AVAILABLE,
+        balcony: true,
+        furnished: true,
+        rentPrice: 500000,
+        rentSchema: APARTMENT_RENT_SCHEMA.MONTHLY,
+        sellPrice: 2000000,
+    },
+]
